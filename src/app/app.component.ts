d0f447f22a97e183c956ee9364b4c45c49054d4b
Rob Birch <rbirchtree@gmail.com>
2019-01-30 18:57:34 -0800

    done on a mact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name:string = '1';
+
+  constructor(){
+    this.name = "Ted"
+  }
+  changeName(name:string):void {
+  this.name = name;
+}
 }
